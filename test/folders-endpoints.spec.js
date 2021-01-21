9d63bb7104481d04d44e03a30fd8c15a4803df7f
Beyonce Knowles <you@example.com>
2021-01-21 14:21:25 -0500

    resubmit

--- test/folders-endpoints.spec.js
+++ test/folders-endpoints.spec.js
@@ -4,7 +4,7 @@ const supertest = require('supertest');
 const app = require('../src/app');
 const { makeFoldersArray } = require('./folders.fixtures');
 
-describe('Folders Endpoints', function() {
+describe('Folders Endpoint', function() {
   let db;
   before('make knex instance', () => {
     db = knex({
